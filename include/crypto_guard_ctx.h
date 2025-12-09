2004ab2af0377a93effdd612fb1d6b28bdfff8f2
PeterRender <webpismo@yahoo.de>
2025-12-09 13:05:59 +0000

    CryptoGuardCtx: реализовал PImpl с помощью std::unique_ptr
    - Заменил сырой указатель на умный указатель с оберткой
      std::experimental::propagate_const для const-correctness
    - Добавил инициализацию/освобождение ресурсов OpenSSL в
      конструкторе/деструкторе Impl
    - Разделил интерфейс и реализацию класса CryptoGuardCtx:
      * Интерфейс не зависит от OpenSSL
      * Реализация полностью скрыта в .cpp файле
    - Сохранено правило пяти: копирование запрещено,
      перемещение разрешено, явно определен деструктор

--- include/crypto_guard_ctx.h
+++ include/crypto_guard_ctx.h
@@ -1,28 +1,35 @@
 #pragma once
 
+#include <experimental/propagate_const>
+#include <memory>  // для подключения std::unique_ptr
 #include <string>
 
 namespace CryptoGuard {
 
 class CryptoGuardCtx {
 public:
-    CryptoGuardCtx() {}
-    ~CryptoGuardCtx() {}
+    // Конструктор по-умолчанию.
+    CryptoGuardCtx();
+    // Деструктор
+    ~CryptoGuardCtx();
 
+    // Удаляем копирующие конструктор и оператор присваивания
     CryptoGuardCtx(const CryptoGuardCtx &) = delete;
     CryptoGuardCtx &operator=(const CryptoGuardCtx &) = delete;
 
+    // Разрешаем перемещающие конструктор и оператор присваивания по-умолчанию
     CryptoGuardCtx(CryptoGuardCtx &&) noexcept = default;
     CryptoGuardCtx &operator=(CryptoGuardCtx &&) noexcept = default;
 
     // API
-    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
-    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
-    std::string CalculateChecksum(std::iostream &inStream) { return "NOT_IMPLEMENTED"; }
+    void EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
+    void DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password);
+    std::string CalculateChecksum(std::iostream &inStream);
 
 private:
-    class Impl;
-    Impl *pImpl_;
+    class Impl;  // предварительное объявление класса, реализующего функционал класса CryptoGuardCtx
+    std::experimental::propagate_const<std::unique_ptr<Impl>>
+        pImpl_;  // умный указатель, владеющий объектом класса Impl
 };
 
 }  // namespace CryptoGuard
--- src/crypto_guard_ctx.cpp
+++ src/crypto_guard_ctx.cpp
@@ -1,4 +1,46 @@
+#include "crypto_guard_ctx.h"
+#include <openssl/evp.h>  // для подключения OpenSSL функций
 
 namespace CryptoGuard {
+
+// Внутренний класс, реализующий функционал класса CryptoGuardCtx
+class CryptoGuardCtx::Impl {
+public:
+    Impl() {
+        // Инициализация библиотеки OpenSSL
+        OpenSSL_add_all_algorithms();
+    }
+
+    ~Impl() {
+        // Освобождение ресурсов, выделенных для библиотеки OpenSSL
+        EVP_cleanup();
+    }
+
+    // Заготовки методов, реализующих API класса CryptoGuardCtx
+    void EncryptFileImpl(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
+    void DecryptFileImpl(std::iostream &inStream, std::iostream &outStream, std::string_view password) {}
+    std::string CalculateChecksumImpl(std::iostream &inStream) { return "NOT_IMPLEMENTED"; }
+};
+
+// Определение конструктора по-умолчанию класса CryptoGuardCtx
+CryptoGuardCtx::CryptoGuardCtx()
+    : pImpl_(std::make_unique<Impl>())  // создаем экземпляр внутреннего класса Impl
+{}
+
+// Явное определение деструктора по-умолчанию класса CryptoGuardCtx
+CryptoGuardCtx::~CryptoGuardCtx() = default;
+
+// Методы-обертки, делегирующие вызовы API класса CryptoGuardCtx внутреннему классу Impl
+void CryptoGuardCtx::EncryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+    pImpl_->EncryptFileImpl(inStream, outStream, password);
+}
+
+void CryptoGuardCtx::DecryptFile(std::iostream &inStream, std::iostream &outStream, std::string_view password) {
+    pImpl_->DecryptFileImpl(inStream, outStream, password);
+}
+
+std::string CryptoGuardCtx::CalculateChecksum(std::iostream &inStream) {
+    return pImpl_->CalculateChecksumImpl(inStream);
+}
 
 }  // namespace CryptoGuard
